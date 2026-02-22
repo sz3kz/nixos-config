046febc28100ffc0d49c2812aed74dfb0a521674
sz3kz <139809645+sz3kz@users.noreply.github.com>
2026-02-22 18:49:54 +0100

    Add nvidia hardware configs + sync home-manager and nixpkgs

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
--- hosts/dell-g3-15-3500/default.nix
+++ hosts/dell-g3-15-3500/default.nix
@@ -3,6 +3,7 @@
     ./hardware-configuration.nix
     ../GENERAL/bluetooth.nix
     ../GENERAL/systemd-boot.nix
+    ../GENERAL/nvidia.nix
   ];
 }
 
