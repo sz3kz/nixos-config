218a5105184afc3d7e11048bd446a4387b4b9d4f
sz3kz <139809645+sz3kz@users.noreply.github.com>
2026-02-23 01:28:48 +0100

    Unify configuration look

--- flake.nix
+++ flake.nix
@@ -1,9 +1,10 @@
-
 {
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+    nixpkgs = {
+      url = "github:nixos/nixpkgs?ref=nixos-25.11";
+    };
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -15,16 +16,18 @@
       myVariables = import /etc/nixos/settings.nix;
     in {
       nixosConfigurations.${myVariables.deviceHostName} = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit myVariables; };
+        specialArgs = { 
+          inherit myVariables; 
+        };
         modules = [ 
           ./system/GENERAL 
           ./hosts/${myVariables.currentDevice}
-          { _module.args.myVariables = myVariables; }
           ./system/users/${myVariables.user_profile}.nix
           home-manager.nixosModules.home-manager
           {
-            home-manager.extraSpecialArgs = { inherit myVariables; };
-
+            home-manager.extraSpecialArgs = { 
+              inherit myVariables; 
+            };
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.${myVariables.username} = import ./home/user_profiles/${myVariables.user_profile}.nix;
