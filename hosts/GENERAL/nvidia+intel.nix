7eafa94964ad903b4e6aa44d40b478940789ea66
sz3kz <139809645+sz3kz@users.noreply.github.com>
2026-02-23 18:09:10 +0100

    Add integrated intel with nvidia support.

---
+++ hosts/GENERAL/nvidia+intel.nix
@@ -0,0 +1,16 @@
+{ config, lib, pkgs, ... }: {
+
+  # 1. Enable OpenGL/Graphics
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  # 2. Load the NVIDIA driver
+  services.xserver.videoDrivers = [ "intel" "nvidia" ];
+  
+  hardware.nvidia = {
+    open = true;
+    modesetting.enable = true;
+  };
+}
--- hosts/dell-g3-15-3500/default.nix
+++ hosts/dell-g3-15-3500/default.nix
@@ -3,7 +3,7 @@
     ./hardware-configuration.nix
     ../GENERAL/bluetooth.nix
     ../GENERAL/systemd-boot.nix
-    ../GENERAL/nvidia.nix
+    ../GENERAL/nvidia+intel.nix
   ];
 }
 
