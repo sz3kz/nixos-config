8108c02dbc7c4bed3a101c84bef8652ccee1550e
sz3kz <139809645+sz3kz@users.noreply.github.com>
2026-03-24 10:05:50 +0100

    Configure sudo to be offensive

--- system/GENERAL/sudo.nix
+++ system/GENERAL/sudo.nix
@@ -1,6 +1,7 @@
-{ declaredSettings, ... }:
+{ pkgs, declaredSettings, ... }:
 
 {
+  security.sudo.package = pkgs.sudo.override { withInsults = true; };
   security.sudo.enable = true;
   users.users.${declaredSettings.username}.extraGroups = [ "wheel" ];
 }
