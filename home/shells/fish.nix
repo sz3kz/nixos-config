3d4bca817252ed0eeb0a1bd1fc8ba46ba0c8df53
sz3kz <139809645+sz3kz@users.noreply.github.com>
2026-04-18 12:49:42 +0200

    Disable cd in favor of zoxide

--- home/shells/fish.nix
+++ home/shells/fish.nix
@@ -3,6 +3,10 @@
 {
   programs.fish = {
     enable = true;
+    shellAliases = {
+      cd = "echo \"fish: Unbased command: cd\" #"; # use zoxide in favor of cd builtin
+    };
+
     interactiveShellInit = ''
       zoxide init fish | source
       direnv hook fish | source
