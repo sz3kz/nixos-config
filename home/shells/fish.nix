94870c7ba426cb4828b685d604cd0afea110252d
sz3kz <139809645+sz3kz@users.noreply.github.com>
2026-04-18 12:08:10 +0200

    Configure zoxide to presist over any fish interractive login shell

--- home/shells/fish.nix
+++ home/shells/fish.nix
@@ -4,6 +4,8 @@
   programs.fish = {
     enable = true;
     interactiveShellInit = ''
+      zoxide init fish | source
+
       set fish_greeting
       echo "You want a good shell? Go fish!"
     '';
