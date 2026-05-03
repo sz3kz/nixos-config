51bce587db14dcc40ec51add69aa48ce2ae03173
sz3kz <139809645+sz3kz@users.noreply.github.com>
2026-05-03 23:26:46 +0200

    Comment out rebuild-time hog of virtualbox

--- system/GENERAL/software/virtualbox.nix
+++ system/GENERAL/software/virtualbox.nix
@@ -3,6 +3,6 @@
 {
   virtualisation.virtualbox.host.enable = true;
   users.extraGroups.vboxusers.members = [ declaredSettings.username ];
-  virtualisation.virtualbox.guest.enable = true;
+  #virtualisation.virtualbox.guest.enable = true;
   virtualisation.virtualbox.guest.dragAndDrop = true;
 }
