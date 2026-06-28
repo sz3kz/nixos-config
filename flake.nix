564187e72ce7298947765c8c1c13137e48727f1a
sz3kz <139809645+sz3kz@users.noreply.github.com>
2026-06-28 13:45:46 +0200

    pin obsidian version so electron need not be rebuilt every time

--- flake.nix
+++ flake.nix
@@ -9,9 +9,12 @@
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    obsidian-electron-overlay = {
+      url = "github:NixOS/nixpkgs/0c88e1f2bdb93d5999019e99cb0e61e1fe2af4c5";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... } @inputs: 
+  outputs = { self, nixpkgs, home-manager, obsidian-electron-overlay, ... } @inputs: 
     let
       declaredSettings= import /etc/nixos/settings.nix;
     in {
@@ -33,6 +36,20 @@
             home-manager.users.${declaredSettings.username} = 
               import ./profiles/${declaredSettings.profile}/home.nix;
           }
+          # Overlay
+          ({ pkgs, ... }: {
+            #nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (pkgs.lib.getName pkg) [
+            #  "obsidian"
+            #];
+            nixpkgs.overlays = [
+              (final: prev: {
+                obsidian = (import obsidian-electron-overlay {
+                  system = prev.system;
+                  config = { allowUnfree = true; };
+                }).obsidian;
+              })
+            ];
+          })
         ];
       };
     };
