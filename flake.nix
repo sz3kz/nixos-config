20c6b9364aa5e0678acebb3dca287b7a4747a47d
sz3kz <139809645+sz3kz@users.noreply.github.com>
2026-08-14 20:52:51 +0200

    minor: configure neovim to use latest tree-sitter

--- flake.nix
+++ flake.nix
@@ -5,6 +5,9 @@
     nixpkgs = {
       url = "github:nixos/nixpkgs?ref=nixos-25.11";
     };
+    nixpkgs-unstable = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -17,15 +20,20 @@
     obsidian-electron-overlay = {
       url = "github:NixOS/nixpkgs/0c88e1f2bdb93d5999019e99cb0e61e1fe2af4c5";
     };
+    neovim-nightly-overlay = {
+      url = "github:nix-community/neovim-nightly-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, plasma-manager, obsidian-electron-overlay, ... } @inputs: 
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, plasma-manager, obsidian-electron-overlay, ... } @inputs: 
     let
       declaredSettings= import /etc/nixos/settings.nix;
     in {
       nixosConfigurations.${declaredSettings.hostname} = nixpkgs.lib.nixosSystem {
         specialArgs = { 
           inherit declaredSettings; 
+          inherit inputs; 
         };
         modules = [ 
           ./hosts/${declaredSettings.host}
@@ -35,6 +43,7 @@
           {
             home-manager.extraSpecialArgs = { 
               inherit declaredSettings; 
+              inherit inputs; 
             };
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
@@ -52,6 +61,7 @@
             #  "obsidian"
             #];
             nixpkgs.overlays = [
+              inputs.neovim-nightly-overlay.overlays.default
               (final: prev: {
                 obsidian = (import obsidian-electron-overlay {
                   system = prev.system;
--- home/text-editors/neovim.nix
+++ home/text-editors/neovim.nix
@@ -1,12 +1,18 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 
+let
+  pkgs-unstable = inputs.nixpkgs-unstable.legacyPackages.${pkgs.system};
+in
 {
   programs.neovim = {
     enable = true;
     extraLuaPackages = ps: [ ps.magick ];
-    extraPackages = with pkgs; [
-      python3Packages.jupytext
-      imagemagick
+
+    extraPackages = [
+      pkgs.python3Packages.jupytext
+      pkgs.imagemagick
+      pkgs.gcc
+      pkgs-unstable.tree-sitter
     ];
     extraPython3Packages = ps: with ps; [
       pynvim
