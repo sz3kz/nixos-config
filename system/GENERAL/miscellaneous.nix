817660e3e679573e149e745e326c58ee6784a388
sz3kz <139809645+sz3kz@users.noreply.github.com>
2026-08-14 04:48:42 +0200

    configure usage of latest firmware

--- system/GENERAL/miscellaneous.nix
+++ system/GENERAL/miscellaneous.nix
@@ -1,4 +1,8 @@
+{ pkgs, ...}:
+
 {
   programs.nix-ld.enable = true;	# needed to run remote clang-format & clang-tidy binaries via pre-commit git hook
   programs.steam.enable = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  hardware.enableRedistributableFirmware = true;
 }
