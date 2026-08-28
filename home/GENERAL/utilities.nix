eb72e185aaa0ac72a265495cf66aa3b0435b4488
sz3kz <139809645+sz3kz@users.noreply.github.com>
2026-08-28 20:12:39 +0200

    refactor: clean up utilities

--- home/GENERAL/utilities.nix
+++ home/GENERAL/utilities.nix
@@ -5,15 +5,13 @@ in
 {
   home.packages = [
     pkgs.fastfetch
-    pkgs.arandr		# kde does not need
-    pkgs.autorandr	# kde does not need
-    pkgs.brightnessctl
-    pkgs.luminance
 
     pkgs-unstable.yt-dlp
     pkgs.spotdl
+
     pkgs.flameshot	# kde does not need
 
+    pkgs.luminance
     pkgs.fuzzel
     pkgs.iwmenu
     pkgs.iwd		# iwmenu needs iwd, configured as a backend for networkmanager for KDE compatibility
